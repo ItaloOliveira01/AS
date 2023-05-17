854ad47f824a917604410eba0cdcbbb938533267
ItaloOliveira01 <132207144+ItaloOliveira01@users.noreply.github.com>
2023-05-16 22:06:50 -0300

    as 16.05

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $('.slider-principal').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 1,
-    adaptiveHeight: true
-  });
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplayspeed: 2000,
+});
               
